19000390092b4effae4fb7bf3aefa1f4ac8f2eda
monolab825 <p.smiles777@gmail.com>
2024-05-02 15:48:29 +0530

    2-5-2024

--- src/app/result/[id]/page.js
+++ src/app/result/[id]/page.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export default function Page({ params }) {
   var moment = require('moment');
   const [url, setUrl] = useState(params.id)
-  const [data, setData] = useState(null)
+
 
 
   const [hostingData, setHostingData] = useState(null)
@@ -15,90 +15,149 @@ export default function Page({ params }) {
   const [robotsData, setRobotsData] = useState(null)
   const [globalranking, setGlobalranking] = useState(null)
   const [reviews, setReviews] = useState(null)
+  const [seo, setSeo] = useState(null)
 
 
 
 
-  useEffect(() => {
-    fetchData()
-  }, [])
-
-  const fetchData = async () => {
-    let result = await fetch(`${process.env.HOST}/api/v1/scan-report`, {
-      method: 'POST',
-      body: JSON.stringify({ url })
-    })
-    result = await result.json()
-    setData(result)
 
 
-    if (result?.success === true) {
-      // hosting data
-      result.data.map((item, index) => {
-        if (item.label === "hosting_details") {
-          setHostingData(item)
-        }
+  // hosting data
+  const fetchHostingData = async () => {
+    try {
+      let result = await fetch(`${process.env.HOST}/api/v1/scan-web-host`, {
+        method: 'POST',
+        body: JSON.stringify({ url })
       })
-      // audience data
-      result.data.map((item, index) => {
-        if (item.label === "audience_engagement_metrics") {
-          setAudienceData(item)
-        }
-      })
-      // brekenlink
-      result.data.map((item, index) => {
-        if (item.label === "broken_links_verification") {
-          setBrokenLinks(item)
-        }
-      })
-
-      // sitemap data
-      result.data.map((item, index) => {
-        if (item.label === "sitemap_analysis") {
-          setSitemapData(item)
-        }
-      })
-
-      // robots data
-      result.data.map((item, index) => {
-        if (item.label === "robots_txt_verification") {
-          setRobotsData(item)
-        }
-      })
-
-      // global ranking
-      result.data.map((item, index) => {
-        if (item.label === "global_ranking") {
-          setGlobalranking(item)
-        }
-      })
-
-      // user reviews
-      result.data.map((item, index) => {
-        if (item.label === "user_reviews") {
-          setReviews(item)
-        }
-      })
-
-
-
-
-
-
-
-
+      result = await result.json()
+      setHostingData(result)
+    } catch (error) {
+      console.log(error);
     }
+    
+  }
 
-
-
-
-
+  // audience data
+  const fetchAudienceData = async () => {
+    try {
+      let result = await fetch(`${process.env.HOST}/api/v1/scan-engagement`, {
+        method: 'POST',
+        body: JSON.stringify({ url })
+      })
+      result = await result.json()
+      setAudienceData(result)
+    } catch (error) {
+      console.log(error);
+    }
 
   }
 
+  // brokenlink data
+  const fetchBrokenLinksData = async () => {
+    try {
+      let result = await fetch(`${process.env.HOST}/api/v1/scan-links`, {
+        method: 'POST',
+        body: JSON.stringify({ url })
+      })
+      result = await result.json()
+      setBrokenLinks(result)
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+
+  // sitemap data
+  const fetchSitemapData = async () => {
+    try {
+      let result = await fetch(`${process.env.HOST}/api/v1/scan-sitemap`, {
+        method: 'POST',
+        body: JSON.stringify({ url })
+      })
+      result = await result.json()
+      setSitemapData(result)
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  // robots data
+  const fetchRobotsData = async () => {
+    try {
+      let result = await fetch(`${process.env.HOST}/api/v1/scan-robots`, {
+        method: 'POST',
+        body: JSON.stringify({ url })
+      })
+      result = await result.json()
+      setRobotsData(result)
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  // rank data
+  const fetchGlobalrankingData = async () => {
+    try {
+      let result = await fetch(`${process.env.HOST}/api/v1/scan-rank`, {
+        method: 'POST',
+        body: JSON.stringify({ url })
+      })
+      result = await result.json()
+      setGlobalranking(result)
+    } catch (error) {
+      console.log(error);
+    }
+    
+  }
+
+  // review data
+  const fetchReviewsData = async () => {
+    try {
+      let result = await fetch(`${process.env.HOST}/api/v1/scan-report`, {
+        method: 'POST',
+        body: JSON.stringify({ url })
+      })
+      result = await result.json()
+      setReviews(result)
+    } catch (error) {
+      console.log(error);
+    }
+    
+  }
+
+  // review data
+  const fetchSeoData = async () => {
+    try {
+      let result = await fetch(`${process.env.HOST}/api/v1/seo-report`, {
+        method: 'POST',
+        body: JSON.stringify({ url })
+      })
+      result = await result.json()
+      setSeo(result)
+    } catch (error) {
+      console.log(error);
+    }
+    
+  }
+
+  useEffect(() => {
+    fetchHostingData()
+    fetchAudienceData()
+    fetchBrokenLinksData()
+    fetchSitemapData()
+    fetchRobotsData()
+    fetchGlobalrankingData()
+    // fetchReviewsData()
+    // fetchSeoData()
+  }, [])
+
   return (
     <>
-      {console.log(data)}
+
 
       <div className='d-flex'>
         <div className='leftCol d-none d-md-block'>
@@ -197,13 +256,11 @@ export default function Page({ params }) {
                         <h2 className='mb-05 fw-medium'>#{globalranking.data.global_rank}</h2>
                         : null
                       }
-
-                      {/* <h5 className='text-danger fw-light'><i className="material-symbols-outlined smallIcon">arrow_drop_down</i>200,814</h5> */}
                     </div>
                     <div>
                       <p className='mb-05'><small><i className="material-symbols-outlined smallIcon">link_off</i> Broken Link</small></p>
 
-                      {brokenLinks !== null ?
+                      {brokenLinks?.success === true ?
                         brokenLinks.data.urls.length === 0 ?
                           <h2 className='mb-05 fw-medium'>PASSED</h2>
                           :
@@ -213,13 +270,13 @@ export default function Page({ params }) {
                       }
 
                     </div>
-                    <div>
-                      <p className='mb-05'><small><i className="material-symbols-outlined smallIcon">editor_choice</i> User Reviews</small></p>
-                      {reviews !== null ?
-                        <h2 className='mb-05 fw-medium'>{reviews.data.rating}</h2>
-                        : null
-                      }
-                    </div>
+                    {/* <div>
+                    <p className='mb-05'><small><i className="material-symbols-outlined smallIcon">editor_choice</i> User Reviews</small></p>
+                    {reviews !== null ?
+                      <h2 className='mb-05 fw-medium'>{reviews.data.rating}</h2>
+                      : null
+                    }
+                  </div> */}
 
                   </div>
                 </div>
@@ -255,24 +312,24 @@ export default function Page({ params }) {
                 audienceData.data.error ? <p>{audienceData.data.error}</p>
                   :
                   <>
-                    <div className='d-flex justify-content-between gap-5 mb-7'>
-                      <div>
+                    <div className='d-flex justify-content-between gap-3 mb-5'>
+                      <div className='whiteBox flex-fill'>
                         <p className="mb-05"><small>Alexa Rank</small></p>
                         <h4 className="mb-05 fw-medium">{audienceData.data.alexa_rank}</h4>
                       </div>
-                      <div>
+                      <div className='whiteBox flex-fill'>
                         <p className="mb-05"><small>Bounce Rate</small></p>
                         <h4 className="mb-05 fw-medium">{audienceData.data.bounce_rate_percentage}%</h4>
                       </div>
-                      <div>
+                      <div className='whiteBox flex-fill'>
                         <p className="mb-05"><small>Pages per Visit</small></p>
                         <h4 className="mb-05 fw-medium">{audienceData.data.daily_pageviews_per_visitor}</h4>
                       </div>
-                      <div>
+                      <div className='whiteBox flex-fill'>
                         <p className="mb-05"><small>Avg Visit Duration</small></p>
                         <h4 className="mb-05 fw-medium">{audienceData.data.daily_time_on_site}</h4>
                       </div>
-                      <div>
+                      <div className='whiteBox flex-fill'>
                         <p className="mb-05"><small>Search Traffic</small></p>
                         <h4 className="mb-05 fw-medium">{audienceData.data.search_traffic_percentage}%</h4>
                       </div>
@@ -282,27 +339,27 @@ export default function Page({ params }) {
                       <div className='col-md-6 col-lg-7 col-xl-8 mb-3'>
                         <h4 className='mb-2'>Compare to</h4>
                         <div className=''>
-                          
-                            {audienceData.data.similar_sites?.map((similar, similarIndex) => (
-                              <div key={similarIndex} className='mb-3'>
-                                <div className="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                                  <div className="progress-bar" style={{width: `${similar.search_traffic_percentage}%`}}>{similar.search_traffic_percentage}%</div>
-                                </div>
-                               <small>{similar.website} </small> 
 
+                          {audienceData.data.similar_sites?.map((similar, similarIndex) => (
+                            <div key={similarIndex} className='mb-3'>
+                              <div className="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                                <div className="progress-bar fw-bold" style={{ width: `${similar.search_traffic_percentage}%` }}>{similar.search_traffic_percentage}%</div>
                               </div>
-                            ))}
-                          
+                              <small>{similar.website} </small>
+
+                            </div>
+                          ))}
+
                         </div>
                       </div>
                       <div className='col-md-6 col-lg-5 col-xl-4'>
                         <h4 className='mb-2'>Web Traffic by Country</h4>
-                        <div className='whiteBox'>
+                        <div className='whiteBox primaryBox'>
                           <ul>
                             {audienceData.data.visitors_by_country?.map((country, countryIndex) => (
                               <li key={countryIndex} className='mb-1'><span className="material-symbols-outlined">
                                 flag_circle
-                              </span> {country.country} <span className='fw-bold text-dark'>{country.site_traffic_percentage}%</span></li>
+                              </span> {country.country} <span className='fw-bold'>{country.site_traffic_percentage}%</span></li>
                             ))}
                           </ul>
 
@@ -321,12 +378,15 @@ export default function Page({ params }) {
               <h2 className='mb-3'><span className="material-symbols-outlined">link_off</span> Broken Links</h2>
               {brokenLinks !== null ?
                 <ul className='brokenLinkList'>
-                  {brokenLinks.data.urls.length !== 0 ? brokenLinks.data.urls.map((item, index) => (
+
+                  {brokenLinks?.success === true ? brokenLinks.data.urls.map((item, index) => (
                     <li key={index}><span className='brokenUrl'>{item.url}</span> <span className='statusCode text-danger fw-medium'>{item.status_code}</span></li>
                   )) : '0 Broken link found'}
                 </ul>
                 : null}
             </div>
+
+            
 
 
 
